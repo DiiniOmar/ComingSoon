40716496713bfe1da35bc8e1a71c62ab0d52fcdc
Diini Omar <96587549+DiiniOmar@users.noreply.github.com>
2024-04-29 21:38:52 +0530

    Code commit

---
+++ Website/forms.py
@@ -0,0 +1,8 @@
+from Website.models import SignUp
+from django import forms
+
+class signUpForm(forms.ModelForm):
+    
+    class Meta:
+        model = SignUp
+        fields = ['name', 'email','phone']
